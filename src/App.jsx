3ca69dfead5cd6fd8834146a87139fe7d9762aae
Ola <oladimejiagbaje@gmail.com>
2026-01-31 09:43:20 +0100

    Implemnt about section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { ScrollTrigger, SplitText } from "gsap/all";
 import Navbar from "./components/Navbar.jsx";
 import Hero from "./components/Hero.jsx";
 import Cocktails from "./components/Cocktails.jsx";
+import About from "./components/About.jsx";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -13,8 +14,8 @@ const App = () => {
       <Navbar />
       <Hero />
       <Cocktails />
-      {/*<About />
-	 <Art />
+      <About />
+      {/*<Art />
 	 <Menu />
 	 <Contact /> */}
     </main>
